befd42b66cfc3417497a2505d612b268bf2d5519
Flynn <wphestiraid@gmail.com>
2018-07-17 23:34:28 +0900

    TypeORM 설정 변경

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,10 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn() id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column() name: string;
 
-    @Column()
-    firstName: string;
-
-    @Column()
-    lastName: string;
-
-    @Column()
-    age: number;
-
+  @Column() age: number;
 }
