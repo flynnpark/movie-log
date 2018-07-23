3fd50b9ec602293caf284f59d84acd1cc9cdee27
Flynn <wphestiraid@gmail.com>
2018-07-23 20:15:36 +0900

    미들웨어 설정

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import { GraphQLServer } from 'graphql-yoga';
+import cors from 'cors';
+import logger from 'morgan';
+import helmet from 'helmet';
 
 class App {
   public app: GraphQLServer;
@@ -6,6 +9,12 @@ class App {
   constructor() {
     this.app = new GraphQLServer({});
   }
+
+  private setMiddlewares = (): void => {
+    this.app.express.use(cors());
+    this.app.express.use(logger('dev'));
+    this.app.express.use(helmet);
+  };
 }
 
 export default new App().app;
