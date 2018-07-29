9d11f4aac89440a4ddb42f3e7c582b35d1e7c025
Flynn <wphestiraid@gmail.com>
2018-07-29 14:03:29 +0900

    프로필 업데이트 Resolver 구현

---
+++ src/api/v1/user/UpdateMyProfile.resolvers.ts
@@ -0,0 +1,38 @@
+import { Resolvers } from '../../../types/resolvers';
+import User from '../../../entity/User';
+import {
+  UpdateMyProfileMutationArgs,
+  UpdateMyProfileResponse,
+} from '../../../types/graph';
+import privateResolver from '../../../utils/privateResolver';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateMyProfile: privateResolver(
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
+        const user: User = req.user;
+        const notNull = {};
+        Object.keys(args).forEach(key => {
+          if (args[key] !== null) {
+            notNull[key] = args[key];
+          }
+        });
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null,
+          };
+        } catch (error) {
+          return { ok: false, error: error.message };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type EmailSignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  EmailSignIn(email: String!, password: String!): EmailSignInResponse!\n  EmailSignUp(email: String!, password: String!, name: String!, birthYear: Int!, profileImage: String): EmailSignUpResponse!\n}\n\ntype EmailSignUpResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype GetMyProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype Query {\n  GetMyProfile: GetMyProfileResponse!\n  user: User\n}\n\ntype User {\n  id: Int!\n  email: String!\n  password: String!\n  name: String!\n  birthYear: Int!\n  profileImage: String\n  createdAt: Int!\n}\n"];
+export const typeDefs = ["type EmailSignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  EmailSignIn(email: String!, password: String!): EmailSignInResponse!\n  EmailSignUp(email: String!, password: String!, name: String!, birthYear: Int!, profileImage: String): EmailSignUpResponse!\n  UpdateMyProfile(password: String, name: String, profileImage: String): UpdateMyProfileResponse!\n}\n\ntype EmailSignUpResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype GetMyProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype Query {\n  GetMyProfile: GetMyProfileResponse!\n  user: User\n}\n\ntype UpdateMyProfileResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype User {\n  id: Int!\n  email: String!\n  password: String!\n  name: String!\n  birthYear: Int!\n  profileImage: String\n  createdAt: Int!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -25,6 +25,7 @@ export interface User {
 export interface Mutation {
   EmailSignIn: EmailSignInResponse;
   EmailSignUp: EmailSignUpResponse;
+  UpdateMyProfile: UpdateMyProfileResponse;
 }
 
 export interface EmailSignInMutationArgs {
@@ -40,6 +41,12 @@ export interface EmailSignUpMutationArgs {
   profileImage: string | null;
 }
 
+export interface UpdateMyProfileMutationArgs {
+  password: string | null;
+  name: string | null;
+  profileImage: string | null;
+}
+
 export interface EmailSignInResponse {
   ok: boolean;
   error: string | null;
@@ -50,4 +57,9 @@ export interface EmailSignUpResponse {
   ok: boolean;
   error: string | null;
   token: string | null;
+}
+
+export interface UpdateMyProfileResponse {
+  ok: boolean;
+  error: string | null;
 }
