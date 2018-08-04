8a705cff24b625941aff4ef2a38d1a1189f38f4f
Flynn <wphestiraid@gmail.com>
2018-08-04 20:02:53 +0900

    유저 프로필 가져오는 부분 추가
    - 자신의 프로필 가져오는 부분 삭제

---
+++ src/api/v1/User/GetUserProfile.resolvers.ts
@@ -0,0 +1,25 @@
+import { Resolvers } from '../../../types/resolvers';
+import privateResolver from '../../../utils/privateResolver';
+import User from '../../../entity/User';
+
+const resolvers: Resolvers = {
+  Query: {
+    GetUserProfile: privateResolver(async (_, { userId }) => {
+      const user = User.findOne({ id: userId });
+      if (user) {
+        return {
+          ok: true,
+          error: null,
+          user,
+        };
+      }
+      return {
+        ok: false,
+        error: 'User not found',
+        user: null,
+      };
+    }),
+  },
+};
+
+export default resolvers;
