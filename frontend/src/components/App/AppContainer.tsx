2e6adaedb7df2f10b774bce2780b23c3ff448451
Flynn <wphestiraid@gmail.com>
2018-08-11 23:56:25 +0900

    Local State 컴포넌트 연결

--- frontend/src/components/App/AppContainer.tsx
+++ frontend/src/components/App/AppContainer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
 import AppPresenter from './AppPresenter';
+import { IS_LOGGED_IN } from './AppQueries';
 
-const AppContainer = () => <AppPresenter isLoggedIn={false} />;
+const AppContainer = ({ data }) => (
+  <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+);
 
-export default AppContainer;
+export default graphql(IS_LOGGED_IN)(AppContainer);
--- frontend/src/components/App/AppQueries.ts
+++ frontend/src/components/App/AppQueries.ts
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import { gql } from 'apollo-boost';
 
 export const IS_LOGGED_IN = gql`
   {
