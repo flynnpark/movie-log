80705bfcbb8caf31ee83086b56a676c650e4caf9
Flynn <wphestiraid@gmail.com>
2018-08-11 23:41:11 +0900

    Apollo-Boost 인증 부분 추가

--- frontend/src/apollo.ts
+++ frontend/src/apollo.ts
@@ -1,6 +1,47 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { Operation } from 'apollo-boost';
 
 const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      auth: {
+        __typename: 'Auth',
+        isLoggedIn: Boolean(localStorage.getItem('jwt'))
+      }
+    },
+    resolvers: {
+      Mutation: {
+        userLogIn: (_, { token }, { cache }) => {
+          localStorage.setItem('jwt', token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: 'Auth',
+                isLoggedIn: true
+              }
+            }
+          });
+          return null;
+        },
+        userLogOut: (_, __, { cache }) => {
+          localStorage.removeItem('jwt');
+          cache.writeData({
+            data: {
+              __typename: 'Auth',
+              isLoggedIn: false
+            }
+          });
+          return null;
+        }
+      }
+    }
+  },
+  request: async (operation: Operation) => {
+    operation.setContext({
+      headers: {
+        'X-JWT': localStorage.getItem('jwt') || ''
+      }
+    });
+  },
   uri: 'http://localhost:4000/graphql'
 });
 
