90f1d6c2a81458966452d319d9962ff46e2e4347
Flynn <wphestiraid@gmail.com>
2018-08-11 23:30:43 +0900

    Apollo-Boost 셋업

--- frontend/src/apollo.ts
+++ frontend/src/apollo.ts
@@ -1,25 +1,7 @@
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql'
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('X-JWT');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  };
-});
+import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
+  uri: 'http://localhost:4000/graphql'
 });
 
 export default client;
