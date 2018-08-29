8109d6034a09ca04dda29e240c9f56bc3507214f
Flynn <wphestiraid@gmail.com>
2018-08-29 22:44:41 +0900

    [FE] 컴포넌트 구조 분리
    - Home 라우트에서 컴포넌트를 부르는 방식으로 변경
    - Navigation 컴포넌트에 있던 레이아웃 제거

--- frontend/src/components/Navigation/NavigationPresenter.tsx
+++ frontend/src/components/Navigation/NavigationPresenter.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { Layout, Input, Avatar } from 'antd';
+import { Input, Avatar } from 'antd';
 import styled from 'styled-components';
-
-const FixedHeader = styled(Layout.Header)`
-  position: fixed;
-  display: flex;
-  background-color: #fff;
-  z-index: 1;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-`;
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -31,30 +21,22 @@ const HeaderSearch = styled.div`
 `;
 
 const SearchField = styled(Input.Search)`
-  width: 600px;
+  width: 640px;
   margin: 8px 8px 8px 8px;
 `;
 
-const Content = styled(Layout.Content)`
-  margin-top: 96px;
-  padding: 0 50px;
-`;
-
 const NavigationPresenter: React.SFC = () => (
-  <Layout>
-    <FixedHeader>
-      <HeaderContainer>
-        <div>Movie.log</div>
-        <div>
-          <Avatar size={32} icon="user" />
-        </div>
-      </HeaderContainer>
-      <HeaderSearch>
-        <SearchField placeholder="영화 제목을 검색해보세요." />
-      </HeaderSearch>
-    </FixedHeader>
-    <Content>Home</Content>
-  </Layout>
+  <React.Fragment>
+    <HeaderContainer>
+      <div>Movie.log</div>
+      <div>
+        <Avatar size={32} icon="user" />
+      </div>
+    </HeaderContainer>
+    <HeaderSearch>
+      <SearchField placeholder="영화 제목을 검색해보세요." />
+    </HeaderSearch>
+  </React.Fragment>
 );
 
 export default NavigationPresenter;
--- frontend/src/routes/Home/HomePresenter.tsx
+++ frontend/src/routes/Home/HomePresenter.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 import Helmet from 'react-helmet';
+import { Layout } from 'antd';
 import Navigation from '../../components/Navigation';
 
+const FixedHeader = styled(Layout.Header)`
+  position: fixed;
+  display: flex;
+  background-color: #fff;
+  z-index: 1;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Content = styled(Layout.Content)`
+  padding-top: 96px;
+`;
+
 const HomePresenter = () => (
-  <React.Fragment>
+  <Layout>
     <Helmet>
       <title>Home | Movie-log</title>
     </Helmet>
-    <Navigation />
-    Home Screen
-  </React.Fragment>
+    <FixedHeader>
+      <Navigation />
+    </FixedHeader>
+    <Content>Home</Content>
+  </Layout>
 );
 
 export default HomePresenter;
