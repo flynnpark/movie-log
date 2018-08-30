31f15e012ca3fed7959d34a539bcbf9c56dce8d6
Flynn <wphestiraid@gmail.com>
2018-08-30 21:40:32 +0900

    [FE] 컴포넌트 구조 변경
    - 로그인 상태에서 네비게이션이 항상 보이도록 수정

--- frontend/src/components/App/AppPresenter.tsx
+++ frontend/src/components/App/AppPresenter.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import { Layout } from 'antd';
 import Home from '../../routes/Home';
 import LogIn from '../../routes/LogIn';
 import SignUp from '../../routes/SignUp';
+import Profile from '../../routes/Profile';
+import Navigation from '../Navigation';
 
 interface IAppPresenterProps {
   isLoggedIn: boolean;
 }
+
+const FixedHeader = styled(Layout.Header)`
+  position: fixed;
+  display: flex;
+  background-color: #fff;
+  z-index: 1;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Content = styled(Layout.Content)`
+  padding-top: 96px;
+  max-width: 640px;
+  width: 100%;
+  margin: 0 auto;
+`;
 
 const AppPresenter: React.SFC<IAppPresenterProps> = ({ isLoggedIn }) => (
   <BrowserRouter>
@@ -23,10 +44,18 @@ const LoggedOutRoutes: React.SFC = () => (
 );
 
 const LoggedInRoutes: React.SFC = () => (
-  <Switch>
-    <Route exact={true} path="/" component={Home} />
-    <Redirect from={'*'} to={'/'} />
-  </Switch>
+  <Layout>
+    <FixedHeader>
+      <Navigation />
+    </FixedHeader>
+    <Content>
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+        <Route exact={true} path="/profile" component={Profile} />
+        <Redirect from={'*'} to={'/'} />
+      </Switch>
+    </Content>
+  </Layout>
 );
 
 export default AppPresenter;
--- frontend/src/routes/Home/HomePresenter.tsx
+++ frontend/src/routes/Home/HomePresenter.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import Helmet from 'react-helmet';
-import { Layout } from 'antd';
-import Navigation from '../../components/Navigation';
-
-const FixedHeader = styled(Layout.Header)`
-  position: fixed;
-  display: flex;
-  background-color: #fff;
-  z-index: 1;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const Content = styled(Layout.Content)`
-  padding-top: 96px;
-  max-width: 640px;
-  width: 100%;
-  margin: 0 auto;
-`;
 
 const HomePresenter = () => (
-  <Layout>
+  <React.Fragment>
     <Helmet>
       <title>Home | Movie-log</title>
     </Helmet>
-    <FixedHeader>
-      <Navigation />
-    </FixedHeader>
-    <Content>Home</Content>
-  </Layout>
+  </React.Fragment>
 );
 
 export default HomePresenter;
