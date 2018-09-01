e39817f837fff78c398cf3e64c412793fbca13f6
Flynn <wphestiraid@gmail.com>
2018-09-01 17:59:35 +0900

    [FE] 회원가입 레이아웃 수정

--- frontend/src/routes/SignUp/SignUpPresenter.tsx
+++ frontend/src/routes/SignUp/SignUpPresenter.tsx
@@ -4,10 +4,8 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import {
   Card,
-  Col,
   Form,
   Input,
-  Row,
   Tooltip,
   Icon,
   DatePicker,
@@ -16,7 +14,7 @@ import {
 } from 'antd';
 import { MutationFn } from 'react-apollo';
 
-const PageWrapper = styled(Row)`
+const PageWrapper = styled.div`
   height: 100vh;
 `;
 
@@ -83,106 +81,104 @@ const SignUpPresenter: React.SFC<IProps> = ({
   );
 
   return (
-    <PageWrapper type="flex" justify="center" align="middle">
+    <PageWrapper>
       <Helmet>
         <title>Sign Up | Movie.log</title>
       </Helmet>
-      <Col span={8}>
-        <Card title="Sign Up">
-          <Form
-            onSubmit={e => {
-              e.preventDefault();
-              onSubmitFn();
-            }}
-          >
-            <Form.Item {...formItemLayout} label="Avatar">
-              <Upload
-                name="avatar"
-                listType="picture-card"
-                showUploadList={false}
-              >
-                {uploadButton}
-              </Upload>
-            </Form.Item>
-            <Form.Item {...formItemLayout} label="E-mail">
-              {getFieldDecorator('email', {
-                rules: [
-                  {
-                    type: 'email',
-                    message: 'The Input is not valid E-mail!'
-                  },
-                  {
-                    required: true,
-                    message: 'Please input your E-mail'
-                  }
-                ]
-              })(<Input name="email" onChange={onChange} />)}
-            </Form.Item>
-            <Form.Item {...formItemLayout} label="Password">
-              {getFieldDecorator('password', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input your password!'
-                  }
-                ]
-              })(<Input type="password" name="password" onChange={onChange} />)}
-            </Form.Item>
-            <Form.Item {...formItemLayout} label="Confirm Password">
-              {getFieldDecorator('confirm', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please confirm your password!'
-                  },
-                  {
-                    validator: compareToFirstPassword
-                  }
-                ]
-              })(<Input type="password" name="confirm" onChange={onChange} />)}
-            </Form.Item>
-            <Form.Item
-              {...formItemLayout}
-              label={
-                <span>
-                  Nickname&nbsp;
-                  <Tooltip title="What do you want others to call you?">
-                    <Icon type="question-circle-o" />
-                  </Tooltip>
-                </span>
-              }
+      <Card title="Sign Up">
+        <Form
+          onSubmit={e => {
+            e.preventDefault();
+            onSubmitFn();
+          }}
+        >
+          <Form.Item {...formItemLayout} label="Avatar">
+            <Upload
+              name="avatar"
+              listType="picture-card"
+              showUploadList={false}
             >
-              {getFieldDecorator('nickname', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input you nickname!',
-                    whitespace: true
-                  }
-                ]
-              })(<Input name="name" onChange={onChange} />)}
-            </Form.Item>
-            <Form.Item {...formItemLayout} label="Birthday">
-              {getFieldDecorator('birthday', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select your birthday!'
-                  }
-                ]
-              })(<DatePicker format={dateFormat} />)}
-            </Form.Item>
-            <Form.Item {...tailFormItemLayout}>
-              <Button type="default" htmlType="button">
-                <Link to="/">Back</Link>
-              </Button>{' '}
-              <Button type="primary" htmlType="submit">
-                Register
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Col>
+              {uploadButton}
+            </Upload>
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="E-mail">
+            {getFieldDecorator('email', {
+              rules: [
+                {
+                  type: 'email',
+                  message: 'The Input is not valid E-mail!'
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail'
+                }
+              ]
+            })(<Input name="email" onChange={onChange} />)}
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Password">
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input your password!'
+                }
+              ]
+            })(<Input type="password" name="password" onChange={onChange} />)}
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Confirm Password">
+            {getFieldDecorator('confirm', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please confirm your password!'
+                },
+                {
+                  validator: compareToFirstPassword
+                }
+              ]
+            })(<Input type="password" name="confirm" onChange={onChange} />)}
+          </Form.Item>
+          <Form.Item
+            {...formItemLayout}
+            label={
+              <span>
+                Nickname&nbsp;
+                <Tooltip title="What do you want others to call you?">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>
+            }
+          >
+            {getFieldDecorator('nickname', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input you nickname!',
+                  whitespace: true
+                }
+              ]
+            })(<Input name="name" onChange={onChange} />)}
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Birthday">
+            {getFieldDecorator('birthday', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please select your birthday!'
+                }
+              ]
+            })(<DatePicker format={dateFormat} />)}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="default" htmlType="button">
+              <Link to="/">Back</Link>
+            </Button>{' '}
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </PageWrapper>
   );
 };
