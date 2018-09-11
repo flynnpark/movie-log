8b54adee8dbd9ce426e75fa089c9972c6e710294
Flynn <wphestiraid@gmail.com>
2018-09-11 23:20:09 +0900

    [FE] 프로필 컴포넌트 Ant Design 사용하도록 변경

--- frontend/src/components/ProfileSection/ProfileSectionPresenter.tsx
+++ frontend/src/components/ProfileSection/ProfileSectionPresenter.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Avatar, Divider } from 'antd';
+import { Card, Avatar } from 'antd';
 
-const ProfileWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const ProfileDetail = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 60px;
-`;
-
-const Name = styled.div`
-  font-size: 32px;
+const ProfileCard = styled(Card)`
+  background-color: transparent;
+  border: none;
+  margin-bottom: 20px;
 `;
 
 const ProfileSectionPresenter = () => (
-  <React.Fragment>
-    <ProfileWrapper>
-      <Avatar size={128} icon="user" />
-      <ProfileDetail>
-        <Name>Flynn</Name>
-      </ProfileDetail>
-    </ProfileWrapper>
-    <Divider />
-  </React.Fragment>
+  <ProfileCard>
+    <Card.Meta
+      avatar={<Avatar size={128} icon={'user'} />}
+      title="Flynn"
+      description="Simple Profile Text"
+    />
+  </ProfileCard>
 );
 
 export default ProfileSectionPresenter;
