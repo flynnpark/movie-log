7e4ff0c1c099b345a8b4db2ed5991f178ead5f3f
Flynn <wphestiraid@gmail.com>
2018-09-20 22:51:44 +0900

    [FE] 홈 컨테이너 작업 시작

--- frontend/src/routes/Home/HomeContainer.tsx
+++ frontend/src/routes/Home/HomeContainer.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import { getNowPlaying } from 'src/types/api';
 import HomePresenter from './HomePresenter';
+import { GET_NOW_PLAYING } from './HomeQueries';
 
-const HomeContainer = () => <HomePresenter />;
+class NowPlayingQueries extends Query<getNowPlaying> {}
+
+class HomeContainer extends Component {
+  public handleNowPlayingRequest = (data: getNowPlaying) => {
+    console.log(data);
+    const { GetNowPlaying } = data;
+    if (GetNowPlaying.ok) {
+      const { movies } = GetNowPlaying;
+      this.setState({
+        nowPlayingMovies: movies
+      });
+    }
+  };
+
+  public render() {
+    return (
+      <NowPlayingQueries
+        query={GET_NOW_PLAYING}
+        onCompleted={this.handleNowPlayingRequest}
+      >
+        {data => <HomePresenter />}
+      </NowPlayingQueries>
+    );
+  }
+}
 
 export default HomeContainer;
