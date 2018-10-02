995143ea237f613c22ab9d46aa116b4e7e828bf5
Flynn <wphestiraid@gmail.com>
2018-10-02 21:00:57 +0900

    [BE] 유저 프로필 결과가 제대로 나오도록 수정

--- backend/src/api/v1/User/GetUserProfile.resolvers.ts
+++ backend/src/api/v1/User/GetUserProfile.resolvers.ts
@@ -5,21 +5,21 @@ import User from '../../../entity/User';
 const resolvers: Resolvers = {
   Query: {
     GetUserProfile: privateResolver(async (_, { userId }) => {
-      const user = User.findOne({ id: userId });
+      const user = await User.findOne({ id: userId });
       if (user) {
         return {
           ok: true,
           error: null,
-          user,
+          user
         };
       }
       return {
         ok: false,
         error: 'User not found',
-        user: null,
+        user: null
       };
-    }),
-  },
+    })
+  }
 };
 
 export default resolvers;
