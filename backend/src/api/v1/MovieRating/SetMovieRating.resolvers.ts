63c601651487be1e5fbb7898152cdde9994cb230
Flynn <wphestiraid@gmail.com>
2018-10-26 20:55:20 +0900

    [BE] 영화 평점 로직 수정
    - MovieRating 스키마 변경
    - 영화 평점 처리에 따른 리턴 데이터 타입 정의

--- backend/src/api/v1/MovieRating/SetMovieRating.resolvers.ts
+++ backend/src/api/v1/MovieRating/SetMovieRating.resolvers.ts
@@ -14,37 +14,53 @@ const resolvers: Resolvers = {
         args: SetMovieRatingMutationArgs,
         { req }
       ): Promise<SetMovieRatingResponse> => {
-        const { movieId, rating } = args;
-        const { user } = req;
-        const movieRating = await MovieRating.findOne({
-          movieId,
-          userId: user.id
-        });
-        if (movieRating) {
-          if (movieRating.rating === rating) {
-            //  점수 삭제
-            await MovieRating.remove(movieRating);
+        try {
+          const { movieId, rating } = args;
+          const { user } = req;
+          const movieRating = await MovieRating.findOne({
+            movieId,
+            userId: user.id
+          });
+          if (movieRating) {
+            if (movieRating.rating === rating) {
+              //  점수 삭제
+              await MovieRating.remove(movieRating);
+              return {
+                ok: true,
+                type: 'remove',
+                error: null,
+                movieRating
+              };
+            } else {
+              // 점수 업데이트
+              movieRating.rating = rating;
+              await MovieRating.save(movieRating);
+              return {
+                ok: true,
+                type: 'update',
+                error: null,
+                movieRating
+              };
+            }
           } else {
-            // 점수 업데이트
-            movieRating.rating = rating;
-            await MovieRating.save(movieRating);
+            const newMovieRating = await MovieRating.create({
+              movieId,
+              userId: user.id,
+              rating
+            }).save();
+            return {
+              ok: true,
+              type: 'create',
+              error: null,
+              movieRating: newMovieRating
+            };
           }
-          // type 구분해서 리턴 값 넘겨야할 듯 함
+        } catch (error) {
           return {
             ok: false,
-            error: 'Rating about this movie exist already',
-            movieRating
-          };
-        } else {
-          const newMovieRating = await MovieRating.create({
-            movieId,
-            userId: user.id,
-            rating
-          }).save();
-          return {
-            ok: true,
-            error: null,
-            movieRating: newMovieRating
+            type: null,
+            error,
+            movieRating: null
           };
         }
       }
