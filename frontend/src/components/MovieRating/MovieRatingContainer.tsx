d74f9959d16dfaa80a523fff50c248a551c67a6d
dalyoon <yeodal89@gmail.com>
2018-11-13 00:38:00 +0900

    async movie rating

--- frontend/src/components/MovieRating/MovieRatingContainer.tsx
+++ frontend/src/components/MovieRating/MovieRatingContainer.tsx
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import MovieRatingPresenter from './MovieRatingPresenter';
+import React, { Component } from "react";
+import MovieRatingPresenter from "./MovieRatingPresenter";
 
 interface IProps {
   rating: number;
   handleClickMovieRating: (rating: number) => void;
 }
 
-class MovieRatingContainer extends Component<IProps> {
-  public render() {
-    const { rating, handleClickMovieRating } = this.props;
-    return (
-      <MovieRatingPresenter
-        rating={rating}
-        handleClickMovieRating={handleClickMovieRating}
-      />
-    );
+interface IState {
+  rating: number;
+}
+
+class MovieRatingContainer extends Component<IProps, IState> {
+  public updateTimeout: any;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rating: 0
+    };
   }
+
+  public componentDidMount() {
+    this.getRating();
+  }
+
+  public render() {
+    const { rating } = this.state;
+    return <MovieRatingPresenter rating={rating} handleChangeRating={this.handleChangeRating} />;
+  }
+
+  public getRating = () => {
+    const { rating } = this.props;
+
+    if (rating === null) {
+      this.setState({ rating: 0 });
+    } else {
+      this.setState({ rating });
+    }
+  };
+
+  public handleChangeRating = (rating: number) => {
+    clearTimeout(this.updateTimeout);
+    const { handleClickMovieRating } = this.props;
+    this.setState({ rating });
+
+    this.updateTimeout = setTimeout(() => handleClickMovieRating(rating), 1500);
+  };
 }
 
 export default MovieRatingContainer;
