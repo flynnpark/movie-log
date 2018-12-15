ac7d47de2541088b907e7621b94b080130733a4e
Flynn <wphestiraid@gmail.com>
2018-12-15 21:02:58 +0900

    [FE] 이후 할 내용 주석으로 저장

--- frontend/src/components/MovieRating/MovieRatingContainer.tsx
+++ frontend/src/components/MovieRating/MovieRatingContainer.tsx
@@ -8,6 +8,10 @@ interface IProps {
 }
 
 class MovieRatingContainer extends Component<IProps> {
+  /**
+   * 별 버튼 클릭시 컴포넌트 자체 rating 숫자만 변경되어야 함
+   * 별점과 날짜 정한 후 Check 버튼을 누를 경우에 저장하도록 수정해야 함
+   */
   public render() {
     const { movieRating, handleClickMovieRating } = this.props;
     if (movieRating) {
