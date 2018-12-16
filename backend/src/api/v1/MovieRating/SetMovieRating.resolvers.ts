181d8fcc31b7a54fe2b6f4a0ba953f8e62260962
Flynn <wphestiraid@gmail.com>
2018-12-16 21:11:12 +0900

    [BE] SetMovieRating 수정
    - 생성만 할 수 있도록 변경
    - 생성시 watchDate까지 받아서 처리하도록 수정

--- backend/src/api/v1/MovieRating/SetMovieRating.resolvers.ts
+++ backend/src/api/v1/MovieRating/SetMovieRating.resolvers.ts
@@ -21,58 +21,24 @@ const resolvers: Resolvers = {
         { req }
       ): Promise<SetMovieRatingResponse> => {
         try {
-          const { id, movieId, rating, watchDate } = args;
+          const { movieId, rating, watchDate } = args;
           const { user } = req;
-          if (id) {
-            // id를 인자로 받은 경우 이전 데이터를 수정하기 위함
-            const movieRating = await MovieRating.findOne({
-              id,
-              userId: user.id
-            });
-            if (movieRating) {
-              // 이전 점수가 있는 경우
-              movieRating.rating = rating;
-              if (watchDate) {
-                // watchDate를 인자로 받은 경우 날짜도 수정
-                movieRating.watchDate = new Date(watchDate);
-              }
-              const updatedMovieRating = await MovieRating.save(movieRating);
-              return {
-                ok: true,
-                type: 'update',
-                error: null,
-                movieRating: {
-                  ...updatedMovieRating,
-                  watchDate: updatedMovieRating.watchDate.toString(),
-                  createdAt: updatedMovieRating.createdAt.toString()
-                }
-              };
+          const newMovieRating = await MovieRating.create({
+            movieId,
+            userId: user.id,
+            rating,
+            watchDate
+          }).save();
+          return {
+            ok: true,
+            type: 'create',
+            error: null,
+            movieRating: {
+              ...newMovieRating,
+              watchDate: newMovieRating.watchDate.toString(),
+              createdAt: newMovieRating.createdAt.toString()
             }
-            // 해당 점수가 존재하지 않으므로 에러 출력
-            return {
-              ok: false,
-              type: 'error',
-              error: 'Rating not exists',
-              movieRating: null
-            };
-          } else {
-            // 이전 점수가 없는 경우
-            const newMovieRating = await MovieRating.create({
-              movieId,
-              userId: user.id,
-              rating
-            }).save();
-            return {
-              ok: true,
-              type: 'create',
-              error: null,
-              movieRating: {
-                ...newMovieRating,
-                watchDate: newMovieRating.watchDate.toString(),
-                createdAt: newMovieRating.createdAt.toString()
-              }
-            };
-          }
+          };
         } catch (error) {
           return {
             ok: false,
