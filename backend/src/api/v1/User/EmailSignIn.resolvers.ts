8864167bb519f610f9f42931dba4edd61ca7fac9
Flynn <wphestiraid@gmail.com>
2018-12-27 20:51:40 +0900

    [BE] 로그인시 유저 객체도 반환하도록 수정

--- backend/src/api/v1/User/EmailSignIn.resolvers.ts
+++ backend/src/api/v1/User/EmailSignIn.resolvers.ts
@@ -2,6 +2,7 @@ import User from '../../../entity/User';
 import {
   EmailSignInMutationArgs,
   EmailSignInResponse,
+  UserExpose
 } from '../../../types/graph';
 import { Resolvers } from '../../../types/resolvers';
 import { createJWT } from '../../../utils/jwt';
@@ -15,26 +16,35 @@ const resolvers: Resolvers = {
       const { email, password } = args;
       try {
         const user = await User.findOne({ email });
+        console.log(user);
         if (!user) {
           return {
             ok: false,
             error: 'No user found with that email',
             token: null,
+            user: null
           };
         }
         const checkPassword = await user.comparePassword(password);
         if (checkPassword) {
           const token = createJWT(user.id);
+          delete user.password;
+          const userExpose: UserExpose = {
+            ...user,
+            createdAt: user.createdAt.toString()
+          };
           return {
             ok: true,
             error: null,
             token,
+            user: userExpose
           };
         } else {
           return {
             ok: false,
             error: 'Wrong password',
             token: null,
+            user: null
           };
         }
       } catch (error) {
@@ -42,10 +52,11 @@ const resolvers: Resolvers = {
           ok: false,
           error: error,
           token: null,
+          user: null
         };
       }
-    },
-  },
+    }
+  }
 };
 
 export default resolvers;
