dd994b82737ccbf9b177ce4d5c42c35446e37826
Flynn <wphestiraid@gmail.com>
2019-01-05 21:45:59 +0900

    [BE] GetMovieDetail 로직 수정
    - 상세한 로직은 주석으로 첨부

--- backend/src/api/v1/Movie/GetMovieDetail.resolvers.ts
+++ backend/src/api/v1/Movie/GetMovieDetail.resolvers.ts
@@ -1,3 +1,4 @@
+import { getConnection } from 'typeorm';
 import { Resolvers } from '../../../types/resolvers';
 import privateResolver from '../../../utils/privateResolver';
 import { getMovieDetail } from '../../../utils/tmdb';
@@ -15,31 +16,64 @@ const resolvers: Resolvers = {
         _: null | undefined,
         { movieId }: GetMovieDetailQueryArgs
       ): Promise<GetMovieDetailResponse> => {
-        const movieDetailFromDB = await Movie.findOne({ id: movieId });
+        const movieRepository = getConnection().getRepository(Movie);
+        const movieDetailFromDB = await movieRepository.findOne({
+          relations: ['genres'],
+          where: { id: movieId }
+        });
+        // DB에 이미 정보가 있을 경우 바로 반환
         if (movieDetailFromDB) {
           return {
             ok: true,
             error: null,
-            movie: null
-          };
-        }
-        const movieDetail = await getMovieDetail(movieId);
-        if (movieDetail) {
-          const genres = movieDetail.genres;
-          if (genres) {
-            genres.forEach(async genre => {
-              await Genre.create(genre).save();
-            });
-          }
-          const movieDetailToDB = await Movie.create(movieDetail).save();
-          return {
-            ok: true,
-            error: null,
             movie: {
-              ...movieDetailToDB,
-              release_date: movieDetailToDB.release_date.toString()
+              ...movieDetailFromDB,
+              release_date: movieDetailFromDB.release_date.toString()
             }
           };
+        }
+
+        // DB에 정보가 없을 경우 TMDB API로 정보 요청
+        const movieDetail = await getMovieDetail(movieId);
+        // TMDB에서 가져온 정보가 있을 경우
+        if (movieDetail) {
+          // 장르 저장
+          const genres = movieDetail.genres;
+          if (genres) {
+            genres.map(async genre => {
+              return await Genre.create(genre).save();
+            });
+          }
+          // 장르 정보를 포함한 영화 정보 저장
+          const movieDetailToDB = new Movie();
+          movieDetailToDB.id = movieDetail.id;
+          movieDetailToDB.title = movieDetail.title;
+          movieDetailToDB.tagline = movieDetail.tagline;
+          movieDetailToDB.poster_path = movieDetail.poster_path;
+          movieDetailToDB.original_language = movieDetail.original_language;
+          movieDetailToDB.original_title = movieDetail.original_title;
+          movieDetailToDB.adult = movieDetail.adult;
+          movieDetailToDB.overview = movieDetail.overview;
+          movieDetailToDB.release_date = new Date(movieDetail.release_date);
+          movieDetailToDB.runtime = movieDetail.runtime;
+          movieDetailToDB.genres = genres;
+          await Movie.create(movieDetailToDB).save();
+          // 다시 DB에 movieId로 정보 검색
+          const movieDetailFromDB = await movieRepository.findOne({
+            relations: ['genres'],
+            where: { id: movieId }
+          });
+          // 해당 movieId에 속하는 데이터가 있을 경우 반환
+          if (movieDetailFromDB) {
+            return {
+              ok: true,
+              error: null,
+              movie: {
+                ...movieDetailFromDB,
+                release_date: movieDetailFromDB.release_date.toString()
+              }
+            };
+          }
         }
         return {
           ok: false,
