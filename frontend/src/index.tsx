9c11be633f959e15959605af0d1d4165cd683c0c
Flynn <wphestiraid@gmail.com>
2019-01-29 20:51:50 +0900

    [FE] LocaleProvicer 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,8 @@ import 'antd/dist/antd.css';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
+import { LocaleProvider } from 'antd';
+import ko_KR from 'antd/lib/locale-provider/ko_KR';
 import client from './apollo';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,7 +11,9 @@ import './GlobalStyles';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <LocaleProvider locale={ko_KR}>
+      <App />
+    </LocaleProvider>
   </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
