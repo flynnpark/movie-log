06b42a96de65666b920a8c4ab9f193a6709775ae
Flynn <wphestiraid@gmail.com>
2019-02-19 23:20:07 +0900

    [FE] 기본 정보 스타일 수정

--- frontend/src/components/MovieInfo/MovieInfoPresenter.tsx
+++ frontend/src/components/MovieInfo/MovieInfoPresenter.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import { getMovieDetail_GetMovieDetail_movie } from 'src/types/local';
+
+const MovieInfoHeader = styled.h1`
+  display: flex;
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 14px;
+`;
+
+const MovieReleasedDate = styled.div`
+  margin-bottom: 10px;
+`;
+
+const MovieOverview = styled.p`
+  line-height: 160%;
+`;
 
 interface IProps {
   movie: getMovieDetail_GetMovieDetail_movie;
 }
 
 const MovieHeaderPresenter: React.FunctionComponent<IProps> = ({ movie }) => (
-  <div>
-    <p>Movie's info is here!</p>
-  </div>
+  <>
+    <MovieInfoHeader>기본 정보</MovieInfoHeader>
+    <MovieReleasedDate>
+      개봉일 {movie.release_date.replace(/-/g, '. ')}
+    </MovieReleasedDate>
+    <MovieOverview>{movie.overview}</MovieOverview>
+  </>
 );
 
 export default MovieHeaderPresenter;
