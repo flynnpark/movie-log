a3e55c66b06e7dae2a24b75ec84923102cc96e6d
Flynn <wphestiraid@gmail.com>
2019-03-11 22:14:02 +0900

    [FE] react-apollo-hooks 패키지 설치

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import 'antd/dist/antd.css';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import ReactDOM from 'react-dom';
 import { LocaleProvider } from 'antd';
 import ko_KR from 'antd/lib/locale-provider/ko_KR';
@@ -11,9 +12,11 @@ import './GlobalStyles';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <LocaleProvider locale={ko_KR}>
-      <App />
-    </LocaleProvider>
+    <ApolloHooksProvider client={client}>
+      <LocaleProvider locale={ko_KR}>
+        <App />
+      </LocaleProvider>
+    </ApolloHooksProvider>
   </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
