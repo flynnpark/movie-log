52eda4325b0c815a60e6a401435542e7751d7114
Flynn <wphestiraid@gmail.com>
2019-03-11 21:06:15 +0900

    [FE] MiniMovieCard에 링크 추가

--- frontend/src/components/MiniMovieCard/MiniMovieCard.tsx
+++ frontend/src/components/MiniMovieCard/MiniMovieCard.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Card } from 'antd';
 import { MovieItem } from 'src/types/local';
+import { Link } from 'react-router-dom';
 
 const MiniMovieCard: React.FunctionComponent<MovieItem> = ({
+  id,
   poster_path,
   title
 }) => (
-  <Card
-    cover={
-      <img
-        src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path}`}
-        alt={title}
-        width={100}
-      />
-    }
-    bodyStyle={{ display: 'none' }}
-  />
+  <Link to={`/movie/${id}`}>
+    <Card
+      cover={
+        <img
+          src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path}`}
+          alt={title}
+          width={100}
+        />
+      }
+      bodyStyle={{ display: 'none' }}
+    />
+  </Link>
 );
 
 export default MiniMovieCard;
